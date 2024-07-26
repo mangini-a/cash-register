7996e349c5ca50294a2954a8e1f4a71b8b8e8993
LucaPesenti <l.pesenti17@studenti.unibg.it>
2024-07-26 17:32:27 +0200

    Creazione progetto e dipendenze

---
+++ code/workspace/MPP_UserInterface/src/main/java/CashRegister/MPP_UserInterface/App.java
@@ -0,0 +1,13 @@
+package CashRegister.MPP_UserInterface;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
---
+++ code/workspace/MPP_UserInterface/src/test/java/CashRegister/MPP_UserInterface/AppTest.java
@@ -0,0 +1,20 @@
+package CashRegister.MPP_UserInterface;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest 
+{
+    /**
+     * Rigorous Test :-)
+     */
+    @Test
+    public void shouldAnswerWithTrue()
+    {
+        assertTrue( true );
+    }
+}

d38e6ef3fd729ce3162fbc0d83fda58eebd432fe
mangini-a <a.mangini@myyahoo.com>
2024-11-07 15:27:06 +0100

    Renewed the Cash Register screen's layout
    This allowed it to be made uniform to the Management screen in terms of appearance.

--- code/mvc-workspace/MPP_BillingSystem/src/main/java/view/colors/AppColors.java
+++ code/mvc-workspace/MPP_BillingSystem/src/main/java/view/colors/AppColors.java
@@ -5,12 +5,13 @@ import java.awt.Color;
 public class AppColors {
 	
 	public static final Color LOGIN_COLOR = new Color(0, 102, 204); // Medium blue
-    public static final Color CASH_REGISTER_COLOR = new Color(0, 153, 0); // Medium green
+    public static final Color CASH_REGISTER_COLOR = Color.DARK_GRAY; // Dark gray
     public static final Color MANAGEMENT_COLOR = new Color(0, 153, 153); // Medium teal
     
     public static final Color ADD_BUTTON_COLOR = new Color(144, 238, 144); // Light green
+    public static final Color ACTION_BUTTON_COLOR = new Color(153, 255, 255); // Light cyan
+    public static final Color CHECKOUT_BUTTON_COLOR = new Color(255, 255, 153); // Light yellow
     public static final Color REMOVE_BUTTON_COLOR = new Color(255, 105, 97); // Light red
-	public static final Color UPDATE_BUTTON_COLOR = new Color(144, 238, 144); // Light green
 	
 	public static final Color HIGHLIGHT_COLOR = new Color(51, 127, 127); // Light teal
 }
